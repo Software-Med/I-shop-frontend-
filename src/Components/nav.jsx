c2f8284923490569627a6cf605cae17053cb463f
Software-Med <141573381+Software-Med@users.noreply.github.com>
2024-08-13 11:24:52 +0100

    Update nav.jsx

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const Nav = () => {
   return (
     <nav>
-      <h2 className="heading">I-Sales</h2>
+      <h3 className="heading">I-Sales</h3>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/blogs">Products</Link></li>
